3b4266ebfc714595d42ace487c5045884f077398
Artsem Savich <asavich@solvd.com>
2021-09-20 16:43:49 +0300

    Add RemoteDriver

--- src/test/java/com/solvd/savich/WebTest.java
+++ src/test/java/com/solvd/savich/WebTest.java
@@ -1,30 +1,34 @@
 package com.solvd.savich;
 
 import com.solvd.savich.gui.pages.SearchPage;
-
 import org.apache.commons.collections.CollectionUtils;
-
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
-import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.remote.RemoteWebDriver;
 import org.openqa.selenium.support.PageFactory;
-
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.testng.Assert;
 import org.testng.annotations.Test;
-
-
-import java.lang.invoke.MethodHandles;
+import java.net.MalformedURLException;
+import java.net.URL;
 import java.util.List;
 
 public class WebTest {
     private static final Logger LOGGER = LoggerFactory.getLogger(WebTest.class);
 
     @Test
-    public void testLogin() {
-        System.setProperty("webdriver.chrome.driver", "/Users/asavich/Documents/selenium/chromedriver93");
-        WebDriver driver = new ChromeDriver();
+    public void testSearchProducts() {
+        ChromeOptions chromeOptions = new ChromeOptions();
+        chromeOptions.setCapability("browserVersion", "93");
+        chromeOptions.setCapability("platformName", "Mac OC");
+        WebDriver driver = null;
+        try {
+            driver = new RemoteWebDriver(new URL("http://localhost:4444/wd/hub"), chromeOptions);
+        } catch (MalformedURLException e) {
+            e.printStackTrace();
+        }
         SearchPage page = PageFactory.initElements(driver, SearchPage.class);
         List<WebElement> goods = page.find("Mac");
 
@@ -33,6 +37,7 @@ public class WebTest {
         for (String n : namesOfTitle) {
             LOGGER.info(n);
         }
-
+        assert driver != null;
+        driver.quit();
     }
 }
